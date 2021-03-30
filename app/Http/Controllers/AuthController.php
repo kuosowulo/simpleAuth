53d744b8f2117d729bbc3e42ee3f84f4bd9c9feb
Jeffary <kuosowulo@gmail.com>
2021-03-30 16:51:20 +0800

    doing - add login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,5 +10,22 @@ class AuthController extends Controller
     public function viewLoginPage()
     {
         return view('loginPage');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            dump('error');
+        }
+
+        return response()->json([
+            'status' => true,
+            'code' => 200,
+            'data' => [
+                'token' => $token
+            ]
+        ]);
     }
 }
